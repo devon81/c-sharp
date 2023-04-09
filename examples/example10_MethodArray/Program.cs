9d3c84f7fbb35b27d6b107cc015af280e4164ca9
Alexandr <hamlack81@gmail.com>
2023-04-09 13:20:33 +0300

    feat some new folder and write code

---
+++ examples/example10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {81,62,53,24,15,66,73,81}; 
+int n = array.Length; 
+int find = 81;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+         Console.WriteLine(index);
+          break;
+    }
+   
+    index = index +1;
+}
+
---
+++ examples/examples08/Program.cs
@@ -0,0 +1,33 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+
+    return result;
+}
+
+int a1 = 100;
+int a2 = 101;
+int a3 = 3;
+int a4 = 4;
+int a5 = 900;
+int a6 = 500;
+int a7 = 7;
+int a8 = 700;
+int a9 = 9;
+
+// int max1 = Max(a1,a2,a3);
+// int max2 = Max(a4,a5,a6);
+// int max3 = Max(a7,a8,a9);
+
+//int max = Max(max1,max2,max3);
+
+int max = Max(
+    Max(a1,a2,a3),
+    Max(a4,a5,a6),
+    Max(a7,a8,a9));
+
+Console.WriteLine(max);
+
+
